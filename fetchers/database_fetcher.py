b3c7ae6c0ffa3caa1361f204509ddb8ebbc16e98
Filip Kubala <69809370+Luckownia@users.noreply.github.com>
2025-07-09 09:53:55 +0200

    Update database_fetcher.py

--- fetchers/database_fetcher.py
+++ fetchers/database_fetcher.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import datetime
 import streamlit as st
 from sqlalchemy import create_engine
 from pymongo import MongoClient
@@ -7,10 +6,15 @@ from pymongo import MongoClient
 def fetch_data_from_db(connection_string, query, db_type, collection_name=None):
     try:
         if db_type == "MongoDB":
+            if not collection_name:
+                st.error("Musisz podać nazwę kolekcji MongoDB!")
+                return pd.DataFrame()
+
             client = MongoClient(connection_string)
             db = client.get_database()
             collection = db[collection_name]
             data = pd.DataFrame(list(collection.find()))
+
             if "_id" in data.columns:
                 data.drop("_id", axis=1, inplace=True)
         else:
@@ -24,5 +28,6 @@ def fetch_data_from_db(connection_string, query, db_type, collection_name=None):
 
         return data
     except Exception as e:
+        print(e)
         st.error(f"Błąd bazy danych: {e}")
         return pd.DataFrame()
